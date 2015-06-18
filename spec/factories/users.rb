7fe59fef00b8b7554f8ca48e3e712f7050cb1aa6
feitian124 <feitian124@gmail.com>
2015-06-18 11:56:43 +0800

    factory girl user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,17 @@
 FactoryGirl.define do
   factory :user do
-    
+    subscribe true
+    sequence(:openid) { |n| "openid#{n}" }
+    sequence(:nickname) { |n| "nickname#{n}" }
+    sex { rand 3 }
+    language "zh_CN"
+    city "city"
+    province "province"
+    country "country"
+    headimgurl "headimgurl"
+    subscribe_time { Time.now }
+    unionid { |n| "unionid#{n}" }
+    remark "remark"
+    groupid 0
   end
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+
+  context "validations" do
+    it { expect(user).to respond_to(:openid) }
+  end
 end
