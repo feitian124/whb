6d290c2357c63af66f0d4481c5133839cb111361
feitian124 <feitian124@gmail.com>
2015-07-07 11:37:06 +0800

    fix album tests

--- spec/factories/albums.rb
+++ spec/factories/albums.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :album do
-    
+    user
+    sequence(:name) { |n| "name#{n}" }
+    sequence(:desc) { |n| "desc#{n}" }
+    music "music"
+    template "template"
   end
-
 end
--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Album, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:album) { create(:album) }
+
+  context "validations" do
+    it { expect(album).to respond_to(:user) }
+  end
 end
