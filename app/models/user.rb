2db51ec08d64e8166caa0851ef70278c1f5e051e
feitian124 <feitian124@gmail.com>
2015-07-07 10:50:43 +0800

    add albums associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :images
+  has_many :albums
 
   validates :nickname, :subscribe_time, presence: true
   validates :openid, uniqueness: true
--- db/migrate/20150707021407_create_albums.rb
+++ db/migrate/20150707021407_create_albums.rb
@@ -1,6 +1,11 @@
 class CreateAlbums < ActiveRecord::Migration
   def change
     create_table :albums do |t|
+      t.belongs_to :user, index: true
+      t.string :name
+      t.string :desc
+      t.string :music
+      t.string :template
 
       t.timestamps null: false
     end
