43ccbc308f65c49148e9dd3a8b53a6de00e83510
feitian124 <feitian124@gmail.com>
2015-07-13 17:41:42 +0800

    show real images

--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -2,11 +2,11 @@ FactoryGirl.define do
   factory :image do
     album
     user
-    sequence(:name) { |n| "name#{n}" }
-    sequence(:src) { |n| "http://localhost:3000/src#{n}" }
+    sequence(:name) { |n| "image#{n}" }
+    sequence(:src) { |n| "src#{n}" }
     sequence(:alt) { |n| "alt#{n}" }
     sequence(:desc) { |n| "desc#{n}" }
-    sequence(:pic_url) { |n| "pic_url#{n}" }
+    sequence(:pic_url) { |n| "http://placehold.it/640X1008/#{(n%10).to_s*6}/000000" }
     sequence(:msg_id) { |n| "msg_id_#{n}" }
     sequence(:media_id) { |n| "media_id_#{n}" }
   end
