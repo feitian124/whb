80fa1ef98ca628102100cef0569b21b4ec7af8b5
feitian124 <feitian124@gmail.com>
2015-07-16 17:03:46 +0800

    fix song spec

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:song) { create(:song) }
+
+  context "validations" do
+    it { expect(song).to respond_to(:src) }
+  end
 end
