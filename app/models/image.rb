f3708c5425fa485e12ab936534bd374e4dd98682
feitian124 <feitian124@gmail.com>
2015-08-06 21:40:45 +0800

    image url works

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,4 +3,16 @@ class Image < ActiveRecord::Base
   has_one :user, :through => :album
 
   mount_uploader :src, ImageUploader
+
+  def url
+    if src.blank?
+      pic_url
+    elsif src.is_a? ImageUploader
+      src.url
+    elsif src.is_a? String
+      src
+    else
+      raise "unknow src"
+    end
+  end
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -16,4 +16,15 @@ RSpec.describe Image, type: :model do
     end
   end
 
+  context "methods" do
+    it "url == pic_url" do
+      expect(image.url).to eq image.pic_url
+    end
+    it "url == src.url" do
+      file = File.open(Rails.root.join('public/images/music-icon.png'))
+      image = Image.new(src: file)
+      expect(image.url).to eq image.src.url
+    end
+  end
+
 end
