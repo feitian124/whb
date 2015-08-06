10c4f8cea0d22ae50f3b7adce8602e7ddfcf95d8
feitian124 <feitian124@gmail.com>
2015-08-06 16:27:21 +0800

    use carrierwave-qiniu 0.1.8

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe Image, type: :model do
   context "validations" do
     it { expect(image).to respond_to(:src) }
   end
+
+  context "carrierwave" do
+    it "does upload image" do
+      file = File.open(Rails.root.join('public/images/music-icon.png'))
+      image = Image.new(src: file)
+      image.save
+      expect(image.errors.count).to eq 0
+    end
+  end
+
 end
