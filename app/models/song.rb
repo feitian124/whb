fca4315679f38dc589a8427d63e66aefd3a1f337
feitian124 <feitian124@gmail.com>
2015-08-07 13:58:20 +0800

    update song path

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,10 @@ class Song < ActiveRecord::Base
   has_many :albums
 
   #mount_uploader :src, SongUploader
+
+  # http://7xkwcm.com1.z0.glb.clouddn.com/musics/see_you_again.m4a
+  def src
+    original = super
+    "http://#{SongUploader.qiniu_bucket_domain}#{original}"
+  end
 end
