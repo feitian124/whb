dd7d426f298397b854db87f5cd2f1e2787682538
feitian124 <feitian124@gmail.com>
2015-08-10 17:01:25 +0800

    mv assets config to assets.rb

--- config/application.rb
+++ config/application.rb
@@ -32,13 +32,6 @@ module Whb
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     #config.i18n.default_locale = "zh-CN"
 
-    # For Foundation 5
-    config.assets.precompile += %w( vendor/modernizr )
-
-    # For bower
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
-
-
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
