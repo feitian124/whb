dfbf20746a69e4c984571dcaf047395fc2c1067c
feitian124 <feitian124@gmail.com>
2015-08-24 18:33:53 +0800

    fetch works

--- app/models/image.rb
+++ app/models/image.rb
@@ -6,39 +6,17 @@ class Image < ActiveRecord::Base
 
   Qiniu.establish_connection! :access_key => WECHAT_CONFIG[:qiniu_access_key],
                               :secret_key => WECHAT_CONFIG[:qiniu_secret_key]
-  @@put_policy = Qiniu::Auth::PutPolicy.new(WECHAT_CONFIG[:qiniu_bucket])
-  @@uptoken = Qiniu::Auth.generate_uptoken(@@put_policy)
 
   def upload
-    tries = 2
-    begin
-      tmp_file = WechatsController.wechat.media self.media_id
-      #tmp_file = File.open(Rails.root.join('public/images/music-icon.png'))
-      #basename = File.basename(tmp_file)
-      uri = "uploads/#{self.class.to_s.underscore}/#{created_at.strftime('%Y%m%d')}/#{id}"
+    uri = "uploads/#{self.class.to_s.underscore}/#{created_at.strftime('%Y%m%d')}/#{id}"
 
-      code, result, response_headers = Qiniu::Storage.upload_with_put_policy(
-        @@put_policy,
-        tmp_file,
-        uri
-      )
+    code, result, response_headers = Qiniu.fetch(WECHAT_CONFIG[:qiniu_bucket], pic_url, uri)
 
-      if "200" == code.to_s
-        self.src = uri
-        self.save!
-        File.delete(tmp_file)
-      else
-        raise "Image upload failed: #{code}, #{result}, #{response_headers}"
-      end
-    rescue => error
-      logger.error "error:#{error.class}, #{error.message}"
-      logger.error "error:#{error.backtrace}"
-      # 可能 token 到期
-      if "401" == code.to_s
-        @@uptoken = Qiniu::Auth.generate_uptoken(@@put_policy)
-        retry unless (tries -= 1).zero?
-        raise
-      end
+    if "200" == code.to_s
+      self.src = uri
+      self.save!
+    else
+      raise "Image upload failed: #{code}, #{result}, #{response_headers}"
     end
   end
 
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -7,24 +7,12 @@ RSpec.describe Image, type: :model do
     it { expect(image).to respond_to(:src) }
   end
 
-  context "carrierwave" do
-    it "does upload image" do
-      file = File.open(Rails.root.join('public/images/music-icon.png'))
-      image = Image.new(src: file)
-      image.save
-      expect(image.errors.count).to eq 0
+  context "methods" do
+    it "#upload" do
+      image.pic_url = 'http://ruby-china-files.b0.upaiyun.com/photo/5982eaaa64f467d9dbda03ad4f40ea27.png'
+      saved = image.upload
+      expect(saved).to eq true
     end
   end
-
-  #context "methods" do
-  #  it "url == pic_url" do
-  #    expect(image.url).to eq image.pic_url
-  #  end
-  #  it "url == src.url" do
-  #    file = File.open(Rails.root.join('public/images/music-icon.png'))
-  #    image = Image.new(src: file)
-  #    expect(image.url).to eq image.src.url
-  #  end
-  #end
 
 end
