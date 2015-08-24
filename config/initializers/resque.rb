71be1610c15365a0685150e714db2383d1b9cd3c
feitian124 <feitian124@gmail.com>
2015-08-24 14:04:27 +0800

    resque initializer

---
+++ config/initializers/resque.rb
@@ -0,0 +1,10 @@
+#Sidekiq.configure_server do |config|
+#  config.redis = { url: 'redis://localhost:6379/12' }
+#end
+#
+#Sidekiq.configure_client do |config|
+#  config.redis = { url: 'redis://localhost:6379/12' }
+#end
+
+Resque.redis = Redis.new(:url => 'http://localhost:6379')
+Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
